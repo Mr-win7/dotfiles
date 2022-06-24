5e26d12fc1b2df369810026c17520ab77b8283a4
Mr_win7 <wwq380820271@163.com>
2022-06-24 12:11:39 +0800

    add auto install plug

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ vim_plug_install() {
   echo "install vim plug."
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  vim -c ':PlugInstall' -c q -c q
 }
 
 echo "------start installation-----"
